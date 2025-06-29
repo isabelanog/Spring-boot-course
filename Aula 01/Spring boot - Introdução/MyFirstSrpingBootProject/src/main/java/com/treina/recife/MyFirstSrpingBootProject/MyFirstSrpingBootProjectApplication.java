ddc65e130e31eeeb8f45c42cb8f614e65e250923
Isabela Nogueira <isabelammn@gmail.com>
2025-06-29 18:37:50 -0300

    MyFirstSpringBootProject module

---
+++ Aula 01/Spring boot - Introdução/MyFirstSrpingBootProject/src/main/java/com/treina/recife/MyFirstSrpingBootProject/MyFirstSrpingBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.treina.recife.MyFirstSrpingBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSrpingBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSrpingBootProjectApplication.class, args);
+	}
+
+}
---
+++ Aula 01/Spring boot - Introdução/MyFirstSrpingBootProject/src/test/java/com/treina/recife/MyFirstSrpingBootProject/MyFirstSrpingBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treina.recife.MyFirstSrpingBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSrpingBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
