2324c992f4f66a0b38dbe43aea7e8b3a3ac16573
Isabela Nogueira <isabelammn@gmail.com>
2025-07-13 16:02:50 -0300

    optimizing imports

--- Projeto/sgp/src/main/java/com/treina/recife/sgp/service/ProjetoService.java
+++ Projeto/sgp/src/main/java/com/treina/recife/sgp/service/ProjetoService.java
@@ -1,15 +1,10 @@
 package com.treina.recife.sgp.service;
 
 import com.treina.recife.sgp.model.Projeto;
-import com.treina.recife.sgp.model.Tarefa;
 import com.treina.recife.sgp.model.Usuario;
-import org.springframework.data.domain.Pageable;
-import com.treina.recife.sgp.repository.ProjetoRepository;
-import com.treina.recife.sgp.repository.TarefaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ProjetoService {
