f66e81b8dc876a11963eb55ca176db31576bd0e6
Isabela Nogueira <isabelammn@gmail.com>
2025-07-19 12:07:20 -0300

    Fix

--- Projeto/sgp/src/main/java/com/treina/recife/sgp/controller/UsuarioController.java
+++ Projeto/sgp/src/main/java/com/treina/recife/sgp/controller/UsuarioController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
@@ -31,18 +32,18 @@ public class UsuarioController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<Usuario>> getUsuarios(@PageableDefault(sort = "userId", direction = Sort.Direction.ASC) Pageable pageable) {
+    public ResponseEntity<Page<Usuario>> getUsuarios(@PageableDefault(sort = "userId",
+            direction = Sort.Direction.ASC) Pageable pageable) {
 
-        Page<Usuario> users = usuarioService.getUsuarios(pageable);
+        Page<Usuario> usuarios = usuarioService.getUsuarios(pageable);
 
-        if (users.isEmpty()) {
+        if (usuarios.isEmpty()) {
             logger.info("Ainda não há usuários cadastrados.");
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(Page.empty());
         } else {
-            return ResponseEntity.status(HttpStatus.OK).body(users);
+            return ResponseEntity.status(HttpStatus.OK).body(usuarios);
         }
     }
-
 
     @GetMapping("/{usuarioId}")
     public ResponseEntity<Object> getUsuarioById(@PathVariable(value = "usuarioId") long usuarioId) {
@@ -50,7 +51,7 @@ public class UsuarioController {
         Optional<Usuario> usuario = usuarioService.getUsuarioById(usuarioId);
 
         if (usuario.isEmpty()) {
-            logger.warn("Usuário não encontrado");
+            logger.error("Usuário não encontrado");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
         } else {
             logger.info(usuario.get().toString());
@@ -74,6 +75,7 @@ public class UsuarioController {
             novoUsuario.setStatus(StatusUsuario.ATIVO);
 
             usuarioService.createUsuario(novoUsuario);
+            logger.info("Usuário {} criado com sucesso", novoUsuario.getUserId());
 
             return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
         }
@@ -86,7 +88,7 @@ public class UsuarioController {
         Optional<Usuario> usuario = usuarioService.getUsuarioById(userId);
 
         if (usuario.isEmpty()) {
-            logger.warn("Usuário não encontrado");
+            logger.error("Usuário não encontrado");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
         } else {
             Usuario usuarioAtualizado = usuario.get();
@@ -98,7 +100,7 @@ public class UsuarioController {
 
             usuarioService.updateUsuario(usuarioAtualizado);
 
-            logger.info("Usuário de id {} criado com sucesso", usuarioAtualizado.getUserId());
+            logger.info("Usuário de ID {} atualizado com sucesso", usuarioAtualizado.getUserId());
 
             return ResponseEntity.status(HttpStatus.OK).body(usuarioAtualizado);
         }
@@ -115,7 +117,7 @@ public class UsuarioController {
         } else {
             usuarioService.deleteUsuario(userId);
             logger.info("Usuário {} deletado com sucesso", userId);
-            return ResponseEntity.status(HttpStatus.OK).body("Usuário deletado com  sucesso");
+            return ResponseEntity.status(HttpStatus.OK).body("Usuário deletado com sucesso");
         }
     }
 
@@ -125,6 +127,7 @@ public class UsuarioController {
 
         Optional<Usuario> usuario = usuarioService.getUsuarioById(userId);
         if (usuario.isEmpty()) {
+            logger.error("Usuário não encontrado.");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
         }
 
@@ -142,10 +145,10 @@ public class UsuarioController {
 
         Usuario usuarioAtualizado = usuario.get();
         usuarioAtualizado.setStatus(novoStatus);
+
         usuarioService.updateUsuario(usuarioAtualizado);
+        logger.info("Status do usuário atualizado com sucesso.");
 
-        return ResponseEntity.ok(usuarioAtualizado);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioAtualizado);
     }
-
-
 }
